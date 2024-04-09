c25b625c7858260133a252599b9878b34f02483e
jennysundstroem <ejyuso-2@student.ltu.se>
2024-04-09 15:13:19 +0200

    code along 2

--- app/src/main/java/com/ltu/m7019e/moviedb/v24/utils/Constants.kt
+++ app/src/main/java/com/ltu/m7019e/moviedb/v24/utils/Constants.kt
@@ -2,6 +2,8 @@ package com.ltu.m7019e.moviedb.v24.utils
 
 object Constants {
     const val POSTER_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val BACKDROP_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
     const val POSTER_IMAGE_WIDTH = "w92"
+    const val BACKDROP_IMAGE_WIDTH = "w1280"
     const val API_KEY = SECRETS.API_KEY
 }
