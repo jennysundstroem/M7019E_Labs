a6e3fd04a9d2ae1090363375c38bd10194747167
jennysundstroem <ejyuso-2@student.ltu.se>
2024-04-10 11:37:49 +0200

    new mock for movies

--- app/src/main/java/com/ltu/m7019e/moviedb/v24/database/Movies.kt
+++ app/src/main/java/com/ltu/m7019e/moviedb/v24/database/Movies.kt
@@ -6,85 +6,86 @@ class Movies {
     fun getMovies(): List<Movie> {
         return listOf<Movie>(
             Movie(
-                1,
-                "Raya and the Last Dragon",
-                "/lPsD10PP4rgUGiGR4CCXA6iY0QQ.jpg",
-                "/9xeEGUZjgiKlI69jwIOi0hjKUIk.jpg",
-                "2021-03-03",
-                "Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. But when an evil force threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, that same evil has returned and it’s up to a lone warrior, Raya, to track down the legendary last dragon to restore the fractured land and its divided people."
+                1011985,
+                "Kung Fu Panda 4",
+                "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+                "/1XDDXPXGiI8id7MrUxK36ke7gkX.jpg",
+                "2024-03-02",
+                "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past."
             ),
             Movie(
-                2,
-                "Sentinelle",
-                "/fFRq98cW9lTo6di2o4lK1qUAWaN.jpg",
-                "/6TPZSJ06OEXeelx1U1VIAt0j9Ry.jpg",
-                "2021-03-05",
-                "Transferred home after a traumatizing combat mission, a highly trained French soldier uses her lethal skills to hunt down the man who hurt her sister."
+                693134,
+                "Dune: Part Two",
+                "/1pdfLvkbY9ohJlCjQH2CZjjYVvJ.jpg",
+                "/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg",
+                "2024-02-27",
+                "Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee."
             ),
             Movie(
-                3,
-                "Zack Snyder's Justice League",
-                "/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg",
-                "/pcDc2WJAYGJTTvRSEIpRZwM3Ola.jpg",
-                "2021-03-18",
-                "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions."
+                823464,
+                "Godzilla x Kong: The New Empire",
+                "/tMefBSflR6PGQLv7WvFPpKLZkyk.jpg",
+                "/j3Z3XktmWB1VhsS8iXNcrR86PXi.jpg",
+                "2024-03-27",
+                "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own."
             ),
             Movie(
-                4,
-                "Tom & Jerry",
-                "/6KErczPBROQty7QoIsaa6wJYXZi.jpg",
-                "/z7HLq35df6ZpRxdMAE0qE3Ge4SJ.jpg",
-                "2021-02-11",
-                "Tom the cat and Jerry the mouse get kicked out of their home and relocate to a fancy New York hotel, where a scrappy employee named Kayla will lose her job if she can’t evict Jerry before a high-class wedding at the hotel. Her solution? Hiring Tom to get rid of the pesky mouse."
+                359410,
+                "Road House",
+                "/bXi6IQiQDHD00JFio5ZSZOeRSBh.jpg",
+                "/oe7mWkvYhK4PLRNAVSvonzyUXNy.jpg",
+                "2024-03-08",
+                "Ex-UFC fighter Dalton takes a job as a bouncer at a Florida Keys roadhouse, only to discover that this paradise is not all it seems."
             ),
             Movie(
-                5,
-                "Below Zero",
-                "/dWSnsAGTfc8U27bWsy2RfwZs0Bs.jpg",
-                "/srYya1ZlI97Au4jUYAktDe3avyA.jpg",
-                "2021-01-29",
-                "When a prisoner transfer van is attacked, the cop in charge must fight those inside and outside while dealing with a silent foe: the icy temperatures."
+                984324,
+                "The Wages of Fear",
+                "/jFK2ZLQUzo9pea0jfMCHDfvWsx7.jpg",
+                "/qekky2LbtT1wtbD5MDgQvjfZQ24.jpg",
+                "2024-03-28",
+                "When an explosion at an oil well threatens hundreds of lives, a crack team is called upon to make a deadly desert crossing with nitroglycerine in tow."
             ),
             Movie(
-                1,
-                "Raya and the Last Dragon",
-                "/lPsD10PP4rgUGiGR4CCXA6iY0QQ.jpg",
-                "/9xeEGUZjgiKlI69jwIOi0hjKUIk.jpg",
-                "2021-03-03",
-                "Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. But when an evil force threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, that same evil has returned and it’s up to a lone warrior, Raya, to track down the legendary last dragon to restore the fractured land and its divided people."
+                935271,
+                "After the Pandemic",
+                "/p1LbrdJ53dGfEhRopG71akfzOVu.jpg",
+                "/9c0lHTXRqDBxeOToVzRu0GArSne.jpg",
+                "2022-03-01",
+                "Set in a post-apocalyptic world where a global airborne pandemic has wiped out 90% of the Earth's population and only the young and immune have endured as scavengers. For Ellie and Quinn, the daily challenges to stay alive are compounded when they become hunted by the merciless Stalkers."
             ),
             Movie(
-                2,
-                "Sentinelle",
-                "/fFRq98cW9lTo6di2o4lK1qUAWaN.jpg",
-                "/6TPZSJ06OEXeelx1U1VIAt0j9Ry.jpg",
-                "2021-03-05",
-                "Transferred home after a traumatizing combat mission, a highly trained French soldier uses her lethal skills to hunt down the man who hurt her sister."
+                1181548,
+                "Heart of the Hunter",
+                "/n726fdyL1dGwt15bY7Nj3XOXc4Q.jpg",
+                "/wUp0bUXaveR40ikBhDgWwNTijuD.jpg",
+                "2024-03-28",
+                "A retired assassin is pulled back into action when his friend uncovers a dangerous conspiracy at the heart of the South African government."
             ),
             Movie(
-                3,
-                "Zack Snyder's Justice League",
-                "/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg",
-                "/pcDc2WJAYGJTTvRSEIpRZwM3Ola.jpg",
-                "2021-03-18",
-                "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions."
+                634492,
+                "Madame Web",
+                "/rULWuutDcN5NvtiZi4FRPzRYWSh.jpg",
+                "/pwGmXVKUgKN13psUjlhC9zBcq1o.jpg",
+                "2024-02-14",
+                "Forced to confront revelations about her past, paramedic Cassandra Webb forges a relationship with three young women destined for powerful futures...if they can all survive a deadly present."
             ),
             Movie(
-                4,
-                "Tom & Jerry",
-                "/6KErczPBROQty7QoIsaa6wJYXZi.jpg",
-                "/z7HLq35df6ZpRxdMAE0qE3Ge4SJ.jpg",
-                "2021-02-11",
-                "Tom the cat and Jerry the mouse get kicked out of their home and relocate to a fancy New York hotel, where a scrappy employee named Kayla will lose her job if she can’t evict Jerry before a high-class wedding at the hotel. Her solution? Hiring Tom to get rid of the pesky mouse."
+                1125311,
+                "Imaginary",
+                "/9u6HEtZJdZDjPGGJq6YEuhPnoan.jpg",
+                "/TGsfNWkASegCfAn6ED1b08a9O6.jpg",
+                "2024-03-06",
+                "When Jessica moves back into her childhood home with her family, her youngest stepdaughter Alice develops an eerie attachment to a stuffed bear named Chauncey she finds in the basement. Alice starts playing games with Chauncey that begin playful and become increasingly sinister. As Alice’s behavior becomes more and more concerning, Jessica intervenes only to realize Chauncey is much more than the stuffed toy bear she believed him to be."
             ),
             Movie(
-                5,
-                "Below Zero",
-                "/dWSnsAGTfc8U27bWsy2RfwZs0Bs.jpg",
-                "/srYya1ZlI97Au4jUYAktDe3avyA.jpg",
-                "2021-01-29",
-                "When a prisoner transfer van is attacked, the cop in charge must fight those inside and outside while dealing with a silent foe: the icy temperatures."
-            ),
+                940551,
+                "Migration",
+                "/ldfCF9RhR40mppkzmftxapaHeTo.jpg",
+                "/2KGxQFV9Wp1MshPBf8BuqWUgVAz.jpg",
+                "2023-12-06",
+                "After a migrating duck family alights on their pond with thrilling tales of far-flung places, the Mallard family embarks on a family road trip, from New England, to New York City, to tropical Jamaica."
+            )
         )
     }
 }
+
